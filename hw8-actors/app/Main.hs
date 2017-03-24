e51f7701c894b265e3d2db44469b64ae69860cba
Ilya Peresadin <pva701@gmail.com>
2017-03-24 20:39:11 +0300

    Added options

--- hw8-actors/app/Main.hs
+++ hw8-actors/app/Main.hs
@@ -1,11 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main where
 
 import           Universum
+
+import           Options   (Args (..), getOptions)
 import           Web       (runWebApp)
 
 main :: IO ()
 main = do
-   runWebApp 3000 2000 [ "http://localhost:3333/yandex/"
-                       , "http://localhost:3333/google/"
-                       , "http://localhost:3333/yahoo/"
-                       ]
+    Args {..} <- getOptions
+    runWebApp port reqTL [ "http://localhost:3333/yandex/"
+                         , "http://localhost:3333/google/"
+                         , "http://localhost:3333/yahoo/"
+                         ]
---
+++ hw8-actors/app/Options.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ApplicativeDo   #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Options
+       ( getOptions
+       , Args (..)
+       ) where
+
+import           Options.Applicative.Simple (Parser, auto, command, help, info, long,
+                                             metavar, option, progDesc, simpleOptions,
+                                             value)
+import           Universum
+
+data Args = Args
+    { reqTL :: !Int -- in ms
+    , port   :: !Word16
+    } deriving (Show)
+
+argsParser :: Parser Args
+argsParser = do
+    reqTL <- option auto (
+        long "req-tl" <>
+        metavar "INT" <>
+        help "Request timeout" <>
+        value 0
+        )
+    port <- option auto (
+        long "port" <>
+        metavar "INT" <>
+        help "Web app port" <>
+        value 3000
+        )
+    pure Args{..}
+
+getOptions :: IO Args
+getOptions = do
+    (res, ()) <-
+        simpleOptions
+            "hw8-app"
+            "App searcher server"
+            "App searcher server"
+            argsParser
+            empty
+    pure res
